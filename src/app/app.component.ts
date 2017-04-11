f134ac502e15f69e2c35b4e9aefca7a5f1377684
weerut panjawattana <weerut.pan@gmail.com>
2017-04-11 13:23:05 +0700

    ds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
+import "bootstrap-datepicker";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app works!';
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+    var that = this;
+    
+    $('#hereiam').datepicker({
+      clearBtn: true,
+      format: 'yyyy-mm-dd',
+      onSelect: function (value, date) {
+        alert(123);
+      },
+      todayBtn: "linked",
+      startView: 0, maxViewMode: 0, minViewMode: 0
+    }).on('onSelect', function (ev) {
+      alert("Change");
+    });
+  }
 }
